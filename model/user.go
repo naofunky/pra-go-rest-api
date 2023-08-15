064bb641a44bc5e7b1898c2f51c548660f3ee6b5
Koki Nanao <minsokosoko0107@gmail.com>
2023-08-15 23:10:30 +0900

    feat:追加
    ユーザーの構造体を定義

--- model/user.go
+++ model/user.go
@@ -1,4 +1,12 @@
 package model
 
+import "time"
+
+// ユーザーの構造体を定義
 type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
