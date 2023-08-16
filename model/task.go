16c26d89dc9a8c1edbdddb62d6a8f1f2ccd0a87a
Koki Nanao <minsokosoko0107@gmail.com>
2023-08-17 01:33:14 +0900

    feat:追加
    ユーザーにレスポンスを返すための構造体を定義

--- model/task.go
+++ model/task.go
@@ -10,3 +10,10 @@ type Task struct {
 	User      User      `json:"user" gorm:"foreignKey:UserID; constraint:OnDelete:CASCADE"`
 	UserID    uint      `json:"user_id" gorm:"not null"`
 }
+
+type TaskResponse struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
