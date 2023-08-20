85b48d0d8279775caf24265bbfa780e99c88ea96
Koki Nanao <minsokosoko0107@gmail.com>
2023-08-20 21:42:57 +0900

    feat:追加
    DB接続情報の追加

---
+++ db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	// 環境変数を取得しにいく
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	// Db接続のためのurl設定
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+
+	// 設定したurlを用いてDB接続
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("connectted")
+	return db
+}
