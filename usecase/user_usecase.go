ed5d0ee7f898caacfe7264940fdeb94490245bf9
Koki Nanao <minsokosoko0107@gmail.com>
2023-08-25 23:28:43 +0900

    usecaseとrepositoryインターフェイスの定義

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // ユーザー関係のユースケースを行うためのインターフェース
@@ -14,4 +16,30 @@ type IUserUsecase interface {
 // ユーザー関係のユースケースを行うための構造体
 type userUsecase struct {
 	ur repository.IUserRepository
+}
+
+// リポジトリをユースケースにDIするためのコンストラクタ
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+// サインイン処理を行うメソッド
+func (uu *userUsecase) Signup(user model.User) (model.UserResponse, error) {
+	// パスワードをハッシュ化
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	// 引数で受け取ったemail情報を元にユーザー情報を作成
+	newUser := model.User{Email: user.Email, Password: string(hash)}
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return model.UserResponse{}, err
+	}
+	// レスポンス用のユーザー情報を作成
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
 }
