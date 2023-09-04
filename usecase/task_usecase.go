0507918cd20cdb70ce513151951f6e8feea2b48e
Koki Nanao <minsokosoko0107@gmail.com>
2023-09-05 00:36:07 +0900

    feat:追加
    usecaseのinterface内にメソッドを追加

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -4,4 +4,8 @@ import "go-rest-api/model"
 
 type ITaskUsecase interface {
 	GetAllTasks(userId uint) ([]model.TaskResponse, error)
+	GetTaskByID(userId uint, taskId uint) (model.TaskResponse, error)
+	CreateTask(task model.Task) (model.TaskResponse, error)
+	UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error)
+	DeleteTask(userId uint, taskId uint) error
 }
