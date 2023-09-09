1da02ecae067615d08e202550fdf2202db79ca77
Koki Nanao <minsokosoko0107@gmail.com>
2023-09-09 15:44:47 +0900

    feat:追加
    routerにエンドポイントにミドルウェアを追加とcontrollerのtaskのエンドポイントを追加

--- router/router.go
+++ router/router.go
@@ -2,17 +2,35 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 
 	// ユーザー関係のルーティング
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+
+	// task関係のエンドポイントのものはグルーピングする
+	t := e.Group("/tasks")
+
+	// jwtのミドルウェアの適用
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+
+	// task関係のエンドポイントを追加
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskByID)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 
 	// echoのインスタンスを返す
 	return e
